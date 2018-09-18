263c1e863bbededbe00204b0a232100a22bd0628
datdta95 <datdta95@gmail.com>
2018-09-18 08:41:23 +0700

    test

---
+++ src/main/java/AlgorithmComplexityTest.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class AlgorithmComplexityTest {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter a string: ");
+        String inputString = scanner.nextLine();
+
+        int[] frequentChar = new int[255];
+        for (int i = 0; i < inputString.length(); i++) {
+            int ascii = (int) inputString.charAt(i);
+            frequentChar[ascii] += 1;
+        }
+
+        int max = 0;
+        char character = (char) 255;
+        for (int i = 0; i < 255; i++) {
+            if (frequentChar[i] > max) {
+                max = frequentChar[i];
+                character= (char) i;
+            }
+        }
+        System.out.println("Phần tử: " + character + " xuất hiện  nhiều nhất với số lần xuất hiện là: " + max);
+    }
+}
